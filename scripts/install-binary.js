c8099098139a9680f8fea9f6580e5e649162a57e
Óscar San José <oscarsj@github.com>
2024-05-31 16:16:15 +0200

    Add CommonJS functionality missing imports

--- scripts/install-binary.js
+++ scripts/install-binary.js
@@ -7,6 +7,8 @@ import { join } from 'path'
 import tempfile from 'tempfile'
 import { existsSync, mkdirSync, renameSync, chmodSync } from 'fs'
 import { DownloaderHelper } from 'node-downloader-helper'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
